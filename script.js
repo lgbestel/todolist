14c1eea754690d571c8cb1e95fae598e2547d6fb
Luiz Guilherme Bestel <luizgbes@gmail.com>
2022-04-28 19:22:23 -0300

    requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-    function addTarefa() {
+function addTarefa() {
     
     let btnTarefa = document.querySelector('#criar-tarefa');
     let iptTarefa = document.querySelector('#texto-tarefa');
     let olTarefas = document.querySelector('#lista-tarefas');
+    
     btnTarefa.addEventListener('click', function() {
         let createLi = document.createElement('li');
         createLi.innerHTML = iptTarefa.value;
+        createLi.className = 'tarefas';
         olTarefas.appendChild(createLi);
         iptTarefa.value = '';
+        
     })
+
 }    addTarefa ();
+
+function selecionarTarefa () {
+
+    let olTarefas = document.querySelector('#lista-tarefas');
+    
+    olTarefas.addEventListener('click', function (event) {
+        let selectedTask = document.querySelector('#selected');
+        event.target.id = 'selected';
+        selectedTask.id = '';
+    })
+} selecionarTarefa ();
